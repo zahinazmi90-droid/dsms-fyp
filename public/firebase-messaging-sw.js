e6d55ea53ce29db2ba48717dc7a06e51e89cce76
Zahin <zahinazmi90@gmail.com>
2026-09-08 01:48:48 +0800

    Fix firebase service worker config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -9,12 +9,12 @@ importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "REPLACE_WITH_NEXT_PUBLIC_FIREBASE_API_KEY",
-  authDomain: "REPLACE_WITH_NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN",
-  projectId: "REPLACE_WITH_NEXT_PUBLIC_FIREBASE_PROJECT_ID",
-  storageBucket: "REPLACE_WITH_NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_WITH_NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID",
-  appId: "REPLACE_WITH_NEXT_PUBLIC_FIREBASE_APP_ID",
+  apiKey: "AIzaSyCNI8Lg_jRDxabYKHSFJ1At7uhAFmEq1wE",
+  authDomain: "dsms-notification.firebaseapp.com",
+  projectId: "dsms-notification",
+  storageBucket: "dsms-notification.firebasestorage.app",
+  messagingSenderId: "912525284571",
+  appId: "1:912525284571:web:880a9fd26e6558fad2a8fe",
 });
 
 const messaging = firebase.messaging();
